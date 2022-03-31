d9af660ba65d6da1ac9f717ff42f099199145fb3
Luucas51 <oudartlucas51@gmail.com>
2022-03-31 15:30:10 +0200

    Boilerplate OK

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ require('./models/dbConfig');
 const postsRoute = require('./routes/postsController');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
+app.use(cors({origin: 'http://localhost:3000'}))
 
 app.use(bodyParser.json())
+
+
 app.use('/posts', postsRoute)
 
 app.listen(5500);
